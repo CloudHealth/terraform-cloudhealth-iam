c93ce1f66e9b6a0386ae9ce79290d38ad4c4be8e
Sean Zou <sean.zou@mulesoft.com>
2021-06-08 00:57:38 -0700

    Add tags support

--- role/role.tf
+++ role/role.tf
@@ -88,6 +88,7 @@ data "template_file" "default-s3-ecs-bucket-policy" {
 resource "aws_iam_role" "cht_iam_role" {
   name = var.role-name
   path = "/"
+  tags = var.tags
 
   assume_role_policy = <<POLICY
 {
@@ -114,6 +115,7 @@ POLICY
 
 resource "aws_iam_policy" "cht_iam_policy" {
   name = var.role-name
+  tags = var.tags
 
   policy = <<POLICY
 {
